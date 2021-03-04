ce9e46c27e108197e2840dec27ee580a8389e4e4
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-03-04 20:52:12 +0000

    added functionality to say what spell the character is trying to cast

--- input.js
+++ input.js
@@ -1,34 +1,36 @@
 const prompt = require('prompt-sync')({sigint: true});
 let Characters = require("./characters");
-const playerCharacterName = Characters.cedric
+//let characterSelect = require("./character-select")
+const playerCharacterName = Characters.cedric // pass in from character select
 
 
-console.log(playerCharacterName.name, playerCharacterName.health, playerCharacterName.spellPower);
+// console.log(playerCharacterName.name, playerCharacterName.health, playerCharacterName.spellPower);
 let chosenSpell = '';
-console.log(playerCharacterName.spells);
-while (chosenSpell == '') {
+console.log(Boolean(''))
+console.log(typeof playerCharacterName.spells);
+while (!chosenSpell) {
     chosenSpell = prompt(`What spell do you want to use? Enter a number between 1 and ${playerCharacterName.spells.length}.`); // bugged for now because of WAN-13
+    if (Number(chosenSpell) > playerCharacterName.spells.length){
+        chosenSpell ='';
+    }
+    switch(Number(chosenSpell)) { // perhaps regex?
+        case 1:
+            console.log(playerCharacterName.spells[0])
+          break;
+        case 2:
+            console.log(playerCharacterName.spells[1])
+          break;
+        case 3:
+            console.log(playerCharacterName.spells[2])
+        break;
+        case 4:
+            console.log(playerCharacterName.spells[3])
+        break;
+        default:
+            console.log("Choose one of the spells.")
+          // code block
+      }
 }
-switch(Number(chosenSpell)) { // perhaps regex?
-    case 1:
-        console.log(Characters.cedric.spells[0])
-      break;
-    case 2:
-        console.log(Characters.cedric.spells[1])
-      break;
-    case 3:
-        console.log(Characters.cedric.spells[2])
-    break;
-    case 4:
-        console.log(Characters.cedric.spells[3])
-    break;
-    default:
-        console.log("Incorrect spell chosen.")
-      // code block
-  }
-
-for (let i of [1,2,3,4,5]){
-    console.log(i)
-}
+console.log(`${playerCharacterName.name} whips out his wand and tries to cast ${playerCharacterName.spells[Number(chosenSpell) - 1].name}!`)
 // test
 // console.log(Characters.cedric.spells[0]['name']) // stupefy
