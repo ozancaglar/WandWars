490b55373fdb08bee1d6dd39dfd92fc975369ac9
IssyYeung <issyyeung@hotmail.co.uk>
2021-03-04 16:23:41 +0000

    spells as class

--- spells.js
+++ spells.js
@@ -1,11 +1,26 @@
-let spells = {
-  stupefy: 1,
-  expelliarmus: 1,
-  impedimenta: 1,
-  sectumsempra: 1,
-  titillando: 1,
-  tarantallegra: 1,
-  fiendfyre: 1,
-};
-
 // "Avada kedavra", "Crucio", "Imperio" for voldemort later
+class Spell {
+  constructor(name, strength, reboundChance) {
+    this.name = name;
+    this.strength = strength;
+    this.reboundChance = reboundChance;
+  }
+}
+
+let stupefy = new Spell(stupefy, 0.9, 0.15);
+let expelliarmus = new Spell(expelliarmus, 0.7, 0.1);
+let impedimenta = new Spell(impedimenta, 0.5, 0.2);
+let sectumsempra = new Spell(sectumsempra, 0.8, 0.15);
+let titillando = new Spell(titillando, 0.4, 0);
+let tarantallegra = new Spell(tarantallegra, 0.6, 0.1);
+let fiendfyre = new Spell(fiendfyre, 1, 0.5);
+
+let spells = [
+  stupefy,
+  expelliarmus,
+  impedimenta,
+  sectumsempra,
+  titillando,
+  tarantallegra,
+  fiendfyre,
+];
