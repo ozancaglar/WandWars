fe5408744175859cf9340537e111ccabcd73b07a
IssyYeung <issyyeung@hotmail.co.uk>
2021-03-04 20:19:37 +0000

    forgot this... basically the same as the last one

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ let character = require("./characters");
 // testing:
 console.log(character.draco.health);
 character.draco.reduceHealth(20);
-console.log(character.draco.health);
+console.log(character.draco.listSpells());
