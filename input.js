63bf827d9e456ae5cbb2a33c4a5e335e237ee579
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-03-04 21:49:46 +0000

    started working on damage calculations within the game. very primitive with no inclusion of rebound / spell failure

--- input.js
+++ input.js
@@ -1,36 +1,35 @@
 const prompt = require('prompt-sync')({sigint: true});
 let Characters = require("./characters");
 //let characterSelect = require("./character-select")
-const playerCharacterName = Characters.cedric // pass in from character select
-
-
+const playerCharacterName = Characters.harry // pass in from character select (Balraj)
+const enemyCharacterName = Characters.draco 
 // console.log(playerCharacterName.name, playerCharacterName.health, playerCharacterName.spellPower);
 let chosenSpell = '';
 console.log(Boolean(''))
 console.log(typeof playerCharacterName.spells);
 while (!chosenSpell) {
     chosenSpell = prompt(`What spell do you want to use? Enter a number between 1 and ${playerCharacterName.spells.length}.`); // bugged for now because of WAN-13
-    if (Number(chosenSpell) > playerCharacterName.spells.length){
+    if (Number(chosenSpell) > playerCharacterName.spells.length || Number(chosenSpell < 1)){
         chosenSpell ='';
-    }
+    } 
     switch(Number(chosenSpell)) { // perhaps regex?
         case 1:
-            console.log(playerCharacterName.spells[0])
-          break;
+            break;
         case 2:
-            console.log(playerCharacterName.spells[1])
-          break;
+            break;
         case 3:
-            console.log(playerCharacterName.spells[2])
-        break;
+            break;
         case 4:
-            console.log(playerCharacterName.spells[3])
-        break;
+            break;
         default:
-            console.log("Choose one of the spells.")
+            console.log(`Enter a number between 1 and ${playerCharacterName.spells.length} to cast a spell.`)
           // code block
       }
 }
+// flavour text
 console.log(`${playerCharacterName.name} whips out his wand and tries to cast ${playerCharacterName.spells[Number(chosenSpell) - 1].name}!`)
+// damage system upon successful cast.
+console.log(enemyCharacterName.health -= playerCharacterName.spells[Number(chosenSpell) - 1].strength * playerCharacterName.spellPower)
+
 // test
 // console.log(Characters.cedric.spells[0]['name']) // stupefy
